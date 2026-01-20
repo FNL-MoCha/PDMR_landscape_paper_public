55ea7092c4a98fe157a9fa49c5041342a51e4180
lchen068 <li.g.chen@gmail.com>
2026-01-20 14:17:43 -0500

    Update PDMR_Fig2A.R

--- Scripts/PDMR_Fig2A.R
+++ Scripts/PDMR_Fig2A.R
@@ -19,7 +19,7 @@ library(ggsci)
 
 
 
-mat <- read.csv("oncoplot_data_pdx.csv", check.names = F, stringsAsFactors = F)
+mat <- read.csv("../DATA/oncoplot_data_pdx.csv", check.names = F, stringsAsFactors = F)
 
 # Function to set the size of the rectangle to be plotted as well as the color based on variat type
 alter_fun = list(
@@ -39,7 +39,7 @@ col = c(AMP = "#de2d26", DEL = "#3182bd",
 
 #Make the top annotations
 #need to sort the annotation file based on the matrix created by oncoplot
-clinical_input <- read.csv("./Supp_tables_pdx_metadata.csv", check.names = F, stringsAsFactors = F)
+clinical_input <- read.csv("../DATA/Supp_tables_pdx_metadata.csv", check.names = F, stringsAsFactors = F)
 
 mat2 <- mat[,2:dim(mat)[2]]
 rownames(mat2) <- mat[,1]
